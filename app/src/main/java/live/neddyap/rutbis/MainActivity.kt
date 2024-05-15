b5588787008715a5c0d7b9eba16c0db9cd288e97
Neddy AP <neddy1298@gmail.com>
2024-05-15 20:08:38 +0700

    cleanup

--- app/src/main/java/live/neddyap/rutbis/MainActivity.kt
+++ app/src/main/java/live/neddyap/rutbis/MainActivity.kt
@@ -3,6 +3,12 @@ package live.neddyap.rutbis
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import live.neddyap.rutbis.databinding.ActivityMainBinding
 import live.neddyap.rutbis.ui.explore.ExploreFragment
@@ -11,12 +17,13 @@ import live.neddyap.rutbis.ui.home.HomeFragment
 import live.neddyap.rutbis.ui.promo.PromoFragment
 import live.neddyap.rutbis.ui.settings.SettingsFragment
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var bottomNavigationView: BottomNavigationView
 
     lateinit var binding: ActivityMainBinding
     private var mLocationPermissionGranted = false
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setContentView(R.layout.activity_maps)
+
         bottomNavigationView = findViewById(R.id.bottom_navigation)
 
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
@@ -57,5 +64,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
