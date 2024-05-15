e696823e7cb3812d133ec00967c937e8aa12ce82
Neddy AP <neddy1298@gmail.com>
2024-05-15 20:10:35 +0700

    reduce time

--- app/src/main/java/live/neddyap/rutbis/SplashActivity.kt
+++ app/src/main/java/live/neddyap/rutbis/SplashActivity.kt
@@ -6,7 +6,7 @@ import android.os.Handler
 import android.content.Intent
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIME_OUT:Long = 3000
+    private val SPLASH_TIME_OUT:Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
