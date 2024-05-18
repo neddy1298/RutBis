8171e38409a37680f8eb5a17c6c4df3351c62bcc
Neddy AP <neddy1298@gmail.com>
2024-05-18 09:59:58 +0700

    minor

--- app/src/main/java/live/neddyap/rutbis/ui/auth/login/LoginFragment.kt
+++ app/src/main/java/live/neddyap/rutbis/ui/auth/login/LoginFragment.kt
@@ -20,7 +20,7 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login, container, false)
+        return inflater.inflate(R.layout.activity_login, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/live/neddyap/rutbis/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/live/neddyap/rutbis/ui/auth/register/RegisterFragment.kt
@@ -20,7 +20,7 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.register, container, false)
+        return inflater.inflate(R.layout.activity_register, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
