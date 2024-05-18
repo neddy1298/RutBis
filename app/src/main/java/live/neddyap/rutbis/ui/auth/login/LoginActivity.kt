f0296ecb295e1838600affde0f70beffc35733a6
Neddy AP <neddy1298@gmail.com>
2024-05-18 10:22:34 +0700

    minor

---
+++ app/src/main/java/live/neddyap/rutbis/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package live.neddyap.rutbis.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import live.neddyap.rutbis.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/live/neddyap/rutbis/ui/auth/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package live.neddyap.rutbis.ui.auth.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import live.neddyap.rutbis.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
