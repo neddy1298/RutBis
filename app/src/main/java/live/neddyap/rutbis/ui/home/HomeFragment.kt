cf126449210b58d644a410420d95eb1c4114f7d2
Neddy AP <neddy1298@gmail.com>
2024-05-26 12:55:54 +0700

    add search activity skeleton

--- app/src/main/java/live/neddyap/rutbis/ui/home/HomeFragment.kt
+++ app/src/main/java/live/neddyap/rutbis/ui/home/HomeFragment.kt
@@ -1,14 +1,17 @@
 package live.neddyap.rutbis.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import live.neddyap.rutbis.databinding.FragmentHomeBinding
 import live.neddyap.rutbis.databinding.ItemHeaderBinding
+import live.neddyap.rutbis.ui.search.SearchActivity
 
 class HomeFragment : Fragment() {
 
@@ -33,9 +36,32 @@ class HomeFragment : Fragment() {
         viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        val searchView = headerBinding.searchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                search(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
 
         return root
 
+    }
+
+    private fun search(query: String?) {
+        // Create an Intent to start SearchActivity
+        val intent = Intent(activity, SearchActivity::class.java).apply {
+            // Pass the query as an extra
+            putExtra("query", query)
+        }
+
+        // Start SearchActivity
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
